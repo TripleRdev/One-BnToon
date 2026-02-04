ce2c09fc206b3dd4a3783de8eec75955eb1b2058
TripleRdev <tripler957@gmail.com>
2026-02-04 17:38:10 +0600

    Restore Data527 ads and remove CSP

--- src/components/ads/units/banner728.ts
+++
@@ -1,5 +0,0 @@
-export const BANNER_728X90 = {
-  adKey: "55df5565f644bb1aefe96eefc0393e90",
-  width: 728,
-  height: 90,
-} as const;
--- src/components/ads/units/index.ts
+++
@@ -1,3 +0,0 @@
-export { BANNER_320X50 } from "./banner320";
-export { BANNER_728X90 } from "./banner728";
-export { NATIVE_300X250 } from "./native300";
